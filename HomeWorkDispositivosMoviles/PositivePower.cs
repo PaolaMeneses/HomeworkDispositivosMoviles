0a1bb139ab40211fb9e915c40b70620899507dde
ANGIE PAOLA MENESES CALDERON <paolamenesescalderon@gmail.com>
2023-09-05 11:46:16 -0500

    Add project files.

---
+++ HomeWorkDispositivosMoviles/PositivePower.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkDispositivosMoviles
+{
+   
+}
+public class PositivePower
+{
+    public PositivePower()
+    {
+
+
+    }
+
+    public void ejecutar()
+
+    {
+
+        bool continuar = true;
+        do
+        {
+            Console.WriteLine("Escribir un numero para elevar al cuadrado");
+            int X = Convert.ToInt16(Console.ReadLine());
+            if (X < 0)
+            {
+                Console.WriteLine("El numero es negativo");
+            }
+            else
+            {
+                double resultado;
+                resultado = Math.Pow(X, 2);
+
+                Console.WriteLine(resultado);
+            }
+
+            Console.WriteLine("Desear salir?" +
+                "\n 1.Si" +
+                "\n 0.No");
+
+            string respuesta = Console.ReadLine();
+            if (respuesta == "1")
+            {
+                continuar = false;
+            }
+
+
+        } while (continuar);
+
+
+    }
+
+
+
+}
---
+++ HomeWorkDispositivosMoviles/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Hola ha ingresado a la tarea de Paola Meneses Calderón");
+
+do
+{
+    Console.WriteLine("Cúal tarea desea revisar ?" +
+                "\n 1.PositivePower" +
+                "\n 2.");
+
+    PositivePower positivePower = new PositivePower();
+    positivePower.ejecutar();
+
+
+
+
+
+
+}
